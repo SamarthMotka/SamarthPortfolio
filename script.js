c91b758e94662eafea3cd3a9ae14d2eb4eb1b42b
SamarthMotka <71966305+SamarthMotka@users.noreply.github.com>
2022-06-26 01:50:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// matrix-rainbow
+
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height =window.innerHeight;
+
+// var rect = document.getElementById('Matrix').getBoundingClientRect();
+// var height = rect.height;
+// var width = rect.width;
+console.log(canvas.height,canvas.width);
+
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 8;
+const columns = 8000;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+	rainDrops[x] = 1;
+}
+
+const draw = () => {
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.fillRect(0, 0, canvas.width,canvas.height );
+	
+	context.fillStyle = '#00f';
+	context.font = fontSize + 'px';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+};
+
+setInterval(draw, 30);
